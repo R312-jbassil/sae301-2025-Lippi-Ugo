9e2ef712358624f28dc37a669e86531befefaec3
Lippi-Ugo <ugo.lippi@edu.univ-fcomte.fr>
2025-11-06 11:13:32 +0100

    action github ajoutée

--- src/utils/pb.ts
+++ src/utils/pb.ts
@@ -1,4 +1,8 @@
 import PocketBase from 'pocketbase';
-
-const pb = new PocketBase('http://127.0.0.1:8090');
+import type { TypedPocketBase } from "./pocketbase-types";
+var path='';
+if(import.meta.env.MODE === 'development')
+    path = 'http://localhost:8090'    //localhost = machine de dev
+else path = 'https://sae301.ugo-lippi.fr:443'   //url du site 
+const pb = new PocketBase(path) as TypedPocketBase;
 export default pb;
